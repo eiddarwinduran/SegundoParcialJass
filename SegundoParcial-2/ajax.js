3618e81e501e499f22ffc88b9a9b53e0a94f700b
eiddarwinduran <eiddarwind734@gmail.com>
2024-11-16 09:09:48 -0400

    color

--- SegundoParcial-2/ajax.js
+++ SegundoParcial-2/ajax.js
@@ -11,3 +11,28 @@ function menu(abrir) {
     }
     ajax.send();
 }
+function pregunta5() {
+	var contenedor = document.getElementById('principal');
+    var his=document.getElementById('historial');
+    his.innerHTML='Presiono 5'
+	fetch('color.html')
+		.then(response => response.text())
+		.then(data => contenedor.innerHTML=data);
+}
+function cambiarColor() {
+    // Obtener el valor del select (sección) y del input (color)
+    const seccion = document.getElementById('seccion').value;
+    const color = document.getElementById('color').value;
+
+    // Cambiar el color de fondo de la sección seleccionada
+    
+    const elemento = document.getElementById(seccion);
+    if(seccion==='bu'){
+        for(var i=1;i<=5;i++){
+            const elemento = document.getElementById(seccion+i);
+            elemento.style.backgroundColor = color;
+        }
+    }else
+      elemento.style.backgroundColor = color;
+    
+}
