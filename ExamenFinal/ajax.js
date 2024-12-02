67da792cb457ac76a6c003ab2ec4f70eeecbc392
eiddarwinduran <eiddarwind734@gmail.com>
2024-12-02 08:33:15 -0400

    q

--- ExamenFinal/ajax.js
+++ ExamenFinal/ajax.js
@@ -7,9 +7,9 @@ function pregunta4() {
 }
 
 function calcularRaices() {
-    const a = parseFloat(document.getElementById('a').value);//recupera el valor de las cajas
-    const b = parseFloat(document.getElementById('b').value);
-    const c = parseFloat(document.getElementById('c').value);
+    const a = parseInt(document.getElementById('a').value);//recupera el valor de las cajas y el parceInt los convierte en un valor numerico
+    const b = parseInt(document.getElementById('b').value);
+    const c = parseInt(document.getElementById('c').value);
     const resultadoDiv = document.getElementById('resultado');
 
     // Calcular el discriminante
@@ -20,18 +20,18 @@ function calcularRaices() {
         // Raíces reales y diferentes
         const x1 = (-b + Math.sqrt(discriminante)) / (2 * a);//esto es una formula "el Math.sqrt(discriminante)calcula la raiz de la sicriminante "
         const x2 = (-b - Math.sqrt(discriminante)) / (2 * a);
-        resultadoDiv.innerHTML = `x1 = ${x1.toFixed(2)} <br> x2 = ${x2.toFixed(2)}`; // aqui esta imprimiendo el resultado 
+        resultadoDiv.innerHTML = `x1 = ${x1} <br> x2 = ${x2}`; // aqui esta imprimiendo el resultado 
     } else if (discriminante === 0) {
         // Raíces reales e iguales
         const x = -b / (2 * a);
-        resultadoDiv.innerHTML = `x1 = ${x.toFixed(2)} <br> x2 = ${x.toFixed(2)} <br>`;
+        resultadoDiv.innerHTML = `x1 = ${x} <br> x2 = ${x} <br>`;
     } else {
         // Raíces complejas
-        const parteReal = (-b / (2 * a)).toFixed(2);
-        const parteImaginaria = (Math.sqrt(-discriminante) / (2 * a)).toFixed(2);
+        const parteReal = (-b / (2 * a)); //se calcula la parte real
+        const parteImaginaria = (Math.sqrt(-discriminante) / (2 * a)); //se calcula la parte imaginaria
         resultadoDiv.innerHTML = `
             x1 = ${parteReal} + <span class="error">${parteImaginaria}i</span> <br>
             x2 = ${parteReal} - <span class="error">${parteImaginaria}i</span>
-        `;
+        `; //aqui imprimimos el resultado
     }
 }
